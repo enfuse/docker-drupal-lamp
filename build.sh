b5330d476f7c904fc2ccaec87a58c752921566c6
Juanjo <juanjo.lopez@biko2.com>
2015-06-25 00:29:09 +0200

    Añado mailhog

--- build.sh
+++ build.sh
@@ -6,7 +6,11 @@ docker build -t drupal-web apache
 docker build -t drupal-db mysql
 docker build -t drupal-mh mailhog
 
+
 docker run -p 3306:3306 --name drupal-db -d drupal-db
-docker run -p 1025:1025 -p 8025 --name drupal-mh -d drupal-mh
+docker run -p 1025:1025 -p 8025:8025 --name drupal-mh -d drupal-mh
 docker run -p 80:80 -v $SCRIPTDIR/www:/var/www/html:rw --link drupal-db:db --link drupal-mh:mh --name drupal-web -d drupal-web
+
 docker exec drupal-web bash /tmp/scripts/drupal-installer.sh
+docker exec drupal-web bash
+
